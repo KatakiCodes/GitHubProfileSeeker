1df75f61b73359b7286197de9da5296cb8d0290c
Nelson Dos Santos <katakicode001@gmail.com>
2025-08-16 18:38:06 +0100

    feat: Create header component

---
+++ src/app/components/header/header.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Output, output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-header',
+  imports: [ReactiveFormsModule],
+  templateUrl: './header.html',
+  styleUrl: './header.css'
+})
+export class Header {
+  @Output() eventEmitter: EventEmitter<string> = new EventEmitter();
+
+  formRquest = new FormGroup({
+    username: new FormControl('',Validators.required)
+  });
+
+  emitEvent(){
+    this.eventEmitter.emit(this.formRquest.get('username')?.value || '');
+  }
+}
